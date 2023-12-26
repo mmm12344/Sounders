3da21379f803bd8416368fb9b73799abfb53ddd1
Mohamed Hesham <Mohamed_hesham23@outlook.com>
2023-12-26 03:19:16 +0200

    added navigation commands added filedialogue made addtrackorplaylistpage improved searchbutton navigation

--- Sounders/Views/AddTrackPage.xaml.cs
+++ Sounders/Views/AddTrackPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +25,22 @@ namespace Sounders.Views
         public AddTrackPage()
         {
             InitializeComponent();
+        }
+
+        private void browseTracksButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Filter = "Audio Files|*.mp3;*.wav;";
+            fileDialog.ShowDialog();
+        }
+
+        private void browsePicture_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Filter = "Image Files|*.png;*.jpeg;";
+            fileDialog.ShowDialog();
         }
     }
 }
---
+++ Sounders/Views/AddTrackorPlaylist.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sounders.Views
+{
+    /// <summary>
+    /// Interaction logic for AddTrackorPlaylist.xaml
+    /// </summary>
+    public partial class AddTrackorPlaylist : Page
+    {
+        public AddTrackorPlaylist()
+        {
+            InitializeComponent();
+        }
+
+        private void createNewPlaylistToPage_Click(object sender, RoutedEventArgs e)
+        {
+            Uri pageFunctionUri = new Uri(@"Views/AddPlaylistPage.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(pageFunctionUri);
+
+        }
+
+        
+
+        private void addTracksToPage_Click(object sender, RoutedEventArgs e)
+        {
+            Uri pageFunctionUri = new Uri(@"Views/AddTrackPage.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(pageFunctionUri);
+           
+        }
+       
+
+    }
+}
