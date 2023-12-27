9f76fabf6529843c1c39774a54c1a4554b39dc0a
Mohamed Hesham <Mohamed_hesham23@outlook.com>
2023-12-27 20:33:06 +0200

    added the new page that make user detremine to add song to whicj playlist

---
+++ Sounders/Views/AddToPlayListPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sounders.Views
+{
+    /// <summary>
+    /// Interaction logic for AddToPlayListPage.xaml
+    /// </summary>
+    public partial class AddToPlayListPage : Page
+    {
+        public AddToPlayListPage()
+        {
+            InitializeComponent();
+        }
+
+        private void openPlayList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void playPlaylistItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
