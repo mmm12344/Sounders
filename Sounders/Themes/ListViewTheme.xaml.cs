568002705793e1192b77c094daa6630b12fc92f8
Mohamed Hesham <Mohamed_hesham23@outlook.com>
2023-12-27 02:50:56 +0200

    made hidden scroll viewer modified list code added Context Menu on Every Item on List

---
+++ Sounders/Themes/ListViewTheme.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sounders.Themes
+{
+    public partial class ListViewTheme : ResourceDictionary
+    {
+        private void addToPlaylistItem_Click(object sender, RoutedEventArgs e)
+        {
+          
+
+        }
+
+        private void likeItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void playItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addToQueueItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
